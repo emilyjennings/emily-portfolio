bdcc6cdd08ee30f9e7ddfee0377a9cb990eb3ce6
github username <github email address>
2019-04-10 23:27:21 -0400

    some components and styling brought over from another site I made

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from "react-router-dom";
+
 import './App.css';
+
+import MainContainer from './containers/MainContainer'
+import About from './components/About'
+import Contact from './components/Contact'
+import GalleryCards from './components/GalleryCards'
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <MainContainer />
+
+          <Route exact path="/" component={GalleryCards} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
