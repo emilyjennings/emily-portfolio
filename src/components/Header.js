a4b6d00b546dca66e09ea444d8ab92aff45c162d
github username <github email address>
2019-04-28 17:25:32 -0400

    home page scrolls nicely

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,71 +6,21 @@ import { Link } from "react-router-dom";
 
 export default class Header extends Component {
 
-//changing the navbar to a long thin line when scrolled
-  navStick = () => {
-
+  scrollFade = () => {
     $(window).scroll(function(){
-      var scrollTop = $(window).scrollTop()
-      if (scrollTop >= 140) {
-        $(".navbar").css(
-          {
-            "width": "100%",
-            "height": "60px",
-            "margin": "0",
-            "text-align": "left",
-            "border-radius": "0",
-          }
-        )
-        $(".navtitle").css(
-          {
-            "display" : "none",
-          }
-        )
-        $('.navlinks a').css(
-          {
-            "font-size": "15px",
-            "display": "inline-block",
-          }
-        )
-        $('hr').css(
-          {
-            "display": "none",
-          }
-        )
-      } else if (scrollTop < 140){
-        $(".navbar").css(
-          {
-            "height": "230px",
-            "text-align": "center",
-            "width": "60%",
-            "margin": "20px 20%",
-            "border-radius": "20px",
-          }
-        )
-        $(".navtitle").css(
-          {
-            "font-size": "60px",
-            "display": "block",
-          }
-        )
-        $('hr').css(
-          {
-            "display": "block",
-            "margin-top": "10px",
-            "margin-bottom": "20px",
-          }
-        )
-        $('.navlinks a').css(
-          {
-            "font-size": "20px",
-          }
-        )
-      }
+      $(".navbar").css("opacity", 1 - $(window).scrollTop() / 250);
+    });
+  }
+
+  navStick = () => {
+    $(window).scroll(function(){
+      $(".navstick").css("opacity", 0 + $(window).scrollTop() / 250)
     });
   }
 
   componentDidMount(){
     {this.navStick()}
+    {this.scrollFade()}
   }
 
   render() {
@@ -81,6 +31,7 @@ export default class Header extends Component {
             <hr></hr>
             <NavLinks />
           </div>
+          <div className="navstick"><NavLinks /></div>
         </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,8 +11,18 @@ export default class Home extends Component {
     $('.navtitle').text("Hi, I'm Emily")
   }
 
+  scrollFade = () => {
+    $(window).scroll(function(){
+      var scrollBottom = $(window).height() - $(window).scrollTop();
+      if (scrollBottom < 10) {
+        $(".homecaption2").animate({'opacity':'1'},5000)
+      }
+    });
+  }
+
   componentDidMount(){
     {this.changeHeader()}
+    {this.scrollFade()}
   }
 
   render() {
@@ -20,9 +30,8 @@ export default class Home extends Component {
       <div className="home">
         <img src={profile} alt=" "/>
           <div class="homecaption">I spent a year and a half with a small child and a full time job teaching myself to code. I deserve to make this representation of me as wacky and wild as I deem necessary.</div>
-        <ScrollAnimation animateIn='fadeIn'>
           <div class="homecaption2">This is me, world. This is real.</div>
-        </ScrollAnimation>
+
       </div>
     );
   };
