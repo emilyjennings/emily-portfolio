7003e3b2228369f31a6cefba1d502320174f8cb6
github username <github email address>
2019-05-07 19:10:29 -0400

    added netlify form stuff to contact page

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
-          <Route path="/projects" component={Projects} />
+          <Route exact path="/projects" component={Projects} />
 
         </div>
       </BrowserRouter>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import $ from 'jquery'
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 export default class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
 
 
 // changes the header subtitle
@@ -11,50 +22,53 @@ export default class Contact extends Component {
     $('.navsubtitle, .navsubtitlestick').show()
   }
 
-// gives a confirmation that the email was sent
-
-  emailForm = () => {
-    $('.submit').click(function() {
-      alert( "email submitted!" );
-    });
-  }
-
   componentDidMount(){
     {this.changeHeader()}
-    {this.emailForm()}
   }
+
+  // Code for the contact form submission given by Netlify documentation
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
 // contact form as per code given by EmailJS, thwe library used for this form
   render() {
+    const { name, email, message } = this.state;
     return (
       <div className="contactpage">
         <div className="empathy">I want to connect with you and exchange stories. I'm not kidding. I live to have conversations with new people. So, leave me a message here.</div>
         <div className="contactcontainer">
           <div className="formcaption">I'll be in touch soon.</div>
-          <form name="thisisemily" method="POST" netlify action="/">
-            email: <input name="email" type="email" name="user_email" className="email_field" />
-            <br></br>
-            name: <input name="name" type="text" name="user_name" className="name_field" />
-            <br></br>
-            message: <textarea name="text" className="message_field" />
-            <br></br>
-            <input type="submit" value="Send" className="submit" />
-          </form>
-          <br></br>
-
-
-          <form name="contact" netlify>
+          <form onSubmit={this.handleSubmit}>
             <p>
-              <label>Name <input type="text" name="name" /></label>
+              <label>
+                Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+              </label>
             </p>
             <p>
-              <label>Email <input type="email" name="email" /></label>
+              <label>
+                Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" value={message} onChange={this.handleChange} />
+              </label>
             </p>
             <p>
               <button type="submit">Send</button>
             </p>
           </form>
-
 
         </div>
       </div>
