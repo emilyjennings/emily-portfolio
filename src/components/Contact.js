f2aea7a684efe7162275b76f0a35a7004e2fe4d3
github username <github email address>
2019-05-07 16:36:40 -0400

    added fake contact form to test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,20 +27,36 @@ export default class Contact extends Component {
 // contact form as per code given by EmailJS, thwe library used for this form
   render() {
     return (
-      <div>
-      <div className="empathy">I want to connect with you and exchange stories. I'm not kidding. I live to have conversations with new people. So, leave me a message here.</div>
-      <div className="contactcontainer">
-        <div className="formcaption">I'll be in touch soon.</div>
-        <form name="emily-contact" method="POST" netlify action="/">
-          email: <input name="email" type="email" name="user_email" className="email_field" />
+      <div className="contactpage">
+        <div className="empathy">I want to connect with you and exchange stories. I'm not kidding. I live to have conversations with new people. So, leave me a message here.</div>
+        <div className="contactcontainer">
+          <div className="formcaption">I'll be in touch soon.</div>
+          <form name="thisisemily" method="POST" netlify action="/">
+            email: <input name="email" type="email" name="user_email" className="email_field" />
+            <br></br>
+            name: <input name="name" type="text" name="user_name" className="name_field" />
+            <br></br>
+            message: <textarea name="text" className="message_field" />
+            <br></br>
+            <input type="submit" value="Send" className="submit" />
+          </form>
           <br></br>
-          name: <input name="name" type="text" name="user_name" className="name_field" />
-          <br></br>
-          message: <textarea name="text" className="message_field" />
-          <br></br>
-          <input type="submit" value="Send" className="submit" />
-        </form>
-      </div>
+
+
+          <form name="contact" netlify>
+            <p>
+              <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+
+
+        </div>
       </div>
     );
   };
