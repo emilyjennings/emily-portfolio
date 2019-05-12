32936ad7bb551ed961dd7e145955ef19e6e7e8fd
github username <github email address>
2019-05-11 21:45:20 -0400

    added most of about page, need skills page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import $ from 'jquery'
 
 import BottomLinks from './BottomLinks'
+import { Link } from "react-router-dom";
+
 
 export default class About extends Component {
 
@@ -56,7 +58,7 @@ timelineFadeIn = () => {
   $(window).scroll(function(){
     var scrollTop = $(window).scrollTop()
     if (scrollTop > 30) {
-      $('.timeline').animate({
+      $('.timeline, .more').animate({
         opacity: '1',
       }, 3000)
     };
@@ -86,34 +88,63 @@ timelineFadeIn = () => {
       <div className="aboutpage">
         <div className="story">I am a human being that likes to do good for others.</div>
 
+        <div className="more">A little more about me</div>
         <div className="timeline">
 
           <div className="dcpic"></div>
-          <div className="beginning">I grew up in the suburbs of DC</div>
+          <div className="beginning">I grew up in the suburbs of DC.</div>
 
 
           <div className="row">
             <div className="event-image" id="abroad"></div>
-            <div className="event" >In high school I was an exchange student in Ukraine and South Africa for long periods</div>
+            <div className="event">In my teens, I had the opportunity to live in Ukraine, South Africa and New Zealand.</div>
           </div>
+
           <div className="row">
-          <div className="event" id="college">I decided to go to college abroad and ended up in Dunedin, New Zealand at the University of Otago. I studied philosophy.</div>
+            <div className="event-image" id="college"></div>
+            <div className="event">In Dunedin, New Zealand at the University of Otago, I studied philosophy.</div>
           </div>
+
           <div className="row">
-          <div className="event" id="youth">After working at some dead-end jobs in DC, I realized I wanted to try teaching. I taught middle school English in North Carolina for one year.</div>
+            <div className="event-image" id="teaching"></div>
+            <div className="event">A few years later, I got into teaching. I loved it.</div>
           </div>
+
           <div className="row">
-          <div className="event" id="jeju">I then moved to Korea to teach ESL. I did that for five years in the beautiful island of Jeju.</div>
+            <div className="event-image" id="jeju"></div>
+            <div className="event">I moved to Korea to teach ESL in 2011. I did that for 5 years in the beautiful island of Jeju.</div>
           </div>
+
           <div className="row">
-          <div className="event" id="philly">Back in the states, I worked at a coding bootcamp in Philly.</div>
+            <div className="event-image" id="philly"></div>
+            <div className="event">Back in the states in 2016, I worked at a coding bootcamp in Philly. I started learning to code.</div>
           </div>
+
           <div className="row">
-          <div className="event" id="dc">I moved back to DC, studied software engineering on my own while working, and now I'm a software engineer.</div>
+            <div className="event-image" id="dc"></div>
+            <div className="event">In 2018, I moved back to DC and studied software engineering on my own while working full time.</div>
           </div>
+
+          <div className="coding"></div>
+          <div className="end">And here we are in 2019: I made this website from scratch.</div>
+
         </div>
 
-        < BottomLinks />
+        <div className="why-title">My 'why'</div>
+        <div className="why">
+          <div className="why-text">I am drawn toward helping others. I like to lift others up. Empathy is a strength I have which allows me to consider all sides of every story and do the right thing. I love to make connections with others and to hear new stories, which is why I enjoy travel and cultures. And I believe that my tech skills will allow me to enhance human connections and help the world come together to make great change in the environment and society.</div>
+        </div>
+
+        <div className="bottom">
+          <div className="row1">
+            <div className="bottom-link"><Link to="https://drive.google.com/file/d/1NpJu_3fS0LiS4zt-FisdtYUKYA2cJ0rM/view?usp=sharing">Resume</Link></div>
+            <div className="bottom-link"><Link to="https://emily-jennings-blog.herokuapp.com/">Blog</Link></div>
+          </div>
+          <div className="row2">
+            <div className="bottom-link"><Link to="">Contact</Link></div>
+            <div className="bottom-link"><Link to="">Map</Link></div>
+          </div>
+        </div>
 
       </div>
     );
