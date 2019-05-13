5881e8e843354944c57d080e4f77ed334516d884
github username <github email address>
2019-05-13 13:26:53 -0400

    some styling

--- src/components/BottomLinks.js
+++ src/components/BottomLinks.js
@@ -19,6 +19,10 @@ export default class BottomLinks extends Component {
           <div className="row-right">
             <div className="bottom-link"><a href="https://emilyjennings.github.io/blog/">Blog</a></div>
           </div>
+          <div className="mobile-row">
+            <div className="bottom-link-left"><a href="https://drive.google.com/file/d/1NpJu_3fS0LiS4zt-FisdtYUKYA2cJ0rM/view?usp=sharing">Resume</a></div>
+            <div className="bottom-link-right"><a href="https://emilyjennings.github.io/blog/">Blog</a></div>
+          </div>
         </div>
       </div>
     );
